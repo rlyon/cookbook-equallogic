9b84745c647e84b160e0b79c78479e8cd3fde25b
Rob Lyon <nosignsoflifehere@gmail.com>
2013-06-12 22:53:02 -0700

    update readme and add support to metadata

--- metadata.rb
+++ metadata.rb
@@ -7,3 +7,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
 depends "sysctl", "= 0.3.1"
+
+%w{ redhat centos }.each do |os|
+  supports os
+end
